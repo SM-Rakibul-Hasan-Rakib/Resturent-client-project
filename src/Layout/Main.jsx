499ff35635c1a6a754e12b41f78583f82245f64f
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-05-18 00:20:42 +0600

    navbar setup loading

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,10 +2,13 @@
 
 import { Outlet } from "react-router-dom";
 import Footer from "../Shared/Footer/Footer";
+import Navbar from "../Shared/Navbar/Navbar";
 
 const Main = () => {
   return (
     <div>
+      {/* navbar */}
+      <Navbar></Navbar>
       {/* outlet */}
       <Outlet></Outlet>
       {/* footer */}
